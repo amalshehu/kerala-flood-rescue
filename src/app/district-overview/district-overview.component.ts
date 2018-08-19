c2cf0f5f5301d83791db3590dcdee53fdf111b06
daliyajohn <daliyajohn75@gmail.com>
2018-08-20 02:01:23 +0530

    Layout UI

--- src/app/district-overview/district-overview.component.ts
+++ src/app/district-overview/district-overview.component.ts
@@ -12,26 +12,26 @@ import {
   styleUrls: ['./district-overview.component.scss']
 })
 export class DistrictOverviewComponent implements OnInit {
-  constructor(private breakpointObserver: BreakpointObserver) {}
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     if (matches) {
+  //       return [
+  //         { title: 'Card 1', cols: 1, rows: 1 },
+  //         { title: 'Card 2', cols: 1, rows: 1 },
+  //         { title: 'Card 3', cols: 1, rows: 1 },
+  //         { title: 'Card 4', cols: 1, rows: 1 }
+  //       ];
+  //     }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  //     return [
+  //       { title: 'Card 1', cols: 2, rows: 1 },
+  //       { title: 'Card 2', cols: 1, rows: 1 },
+  //       { title: 'Card 3', cols: 1, rows: 2 },
+  //       { title: 'Card 4', cols: 1, rows: 1 }
+  //     ];
+  //   })
+  // );
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit() {}
 }
