ee1018a6c3902b4a4aa6495b6c7d0a5ec48ac8e8
daliyajohn <daliyajohn75@gmail.com>
2018-08-20 05:33:10 +0530

    Responsive fix in dashboard

--- src/app/district-overview/district-overview.component.ts
+++ src/app/district-overview/district-overview.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
-import {
-  Breakpoints,
-  BreakpointState,
-  BreakpointObserver
-} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-district-overview',
@@ -12,26 +7,7 @@ import {
   styleUrls: ['./district-overview.component.scss']
 })
 export class DistrictOverviewComponent implements OnInit {
-  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-  //   map(({ matches }) => {
-  //     if (matches) {
-  //       return [
-  //         { title: 'Card 1', cols: 1, rows: 1 },
-  //         { title: 'Card 2', cols: 1, rows: 1 },
-  //         { title: 'Card 3', cols: 1, rows: 1 },
-  //         { title: 'Card 4', cols: 1, rows: 1 }
-  //       ];
-  //     }
-
-  //     return [
-  //       { title: 'Card 1', cols: 2, rows: 1 },
-  //       { title: 'Card 2', cols: 1, rows: 1 },
-  //       { title: 'Card 3', cols: 1, rows: 2 },
-  //       { title: 'Card 4', cols: 1, rows: 1 }
-  //     ];
-  //   })
-  // );
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {}
 
   ngOnInit() {}
 }
