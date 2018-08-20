1c4b44684cd877c660194678516e89d597fff765
daliyajohn <daliyajohn75@gmail.com>
2018-08-20 08:53:51 +0530

    Update form UI fix

--- src/app/update-camp/update-camp.component.ts
+++ src/app/update-camp/update-camp.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-update-camp',
   templateUrl: './update-camp.component.html',
   styleUrls: ['./update-camp.component.scss']
 })
 export class UpdateCampComponent implements OnInit {
+  // myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
   constructor() {}
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Kollam' },
+    { value: 'steak-0', viewValue: 'Kottayam' }
+  ];
 
-  ngOnInit() {}
+  ngOnInit() {
+    // this.filteredOptions = this.myControl.valueChanges.pipe(
+    //   startWith(''),
+    //   map(value => this._filter(value))
+    // );
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 }
--- src/app/update-camp/update-camp.module.ts
+++ src/app/update-camp/update-camp.module.ts
@@ -4,13 +4,19 @@ import { UpdateCampRouteRoutingModule } from 'src/app/update-camp/update-camp-ro
 import { UpdateCampComponent } from 'src/app/update-camp/update-camp.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     CommonModule,
     UpdateCampRouteRoutingModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatToolbarModule
   ],
   declarations: [UpdateCampComponent]
 })
